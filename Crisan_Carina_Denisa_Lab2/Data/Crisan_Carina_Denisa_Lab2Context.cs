1d3ef3550adab4182687eb92052b6fe2d5a154a6
Carina <carina_cary2001@yahoo.com>
2023-11-03 09:06:28 +0200

    Add project files.

---
+++ Crisan_Carina_Denisa_Lab2/Data/Crisan_Carina_Denisa_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Crisan_Carina_Denisa_Lab2.Models;
+
+namespace Crisan_Carina_Denisa_Lab2.Data
+{
+    public class Crisan_Carina_Denisa_Lab2Context : DbContext
+    {
+        public Crisan_Carina_Denisa_Lab2Context (DbContextOptions<Crisan_Carina_Denisa_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Crisan_Carina_Denisa_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Crisan_Carina_Denisa_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Crisan_Carina_Denisa_Lab2/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace Crisan_Carina_Denisa_Lab2.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
